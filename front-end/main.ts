3110555716b6dbed2ffbff9e3cc4782e1442354d
Hiroji <joegilbertdev@outlook.com>
2022-11-08 15:47:28 +0000

    successfully loaded jquery

---
+++ front-end/main.ts
@@ -0,0 +1,8 @@
+//import $ from 'jquery';
+
+$(function(){
+    $("#included__header").load("./components/header.html"); 
+  });
+
+
+// this helps TypeScript to understand jQuery best !!!  otherwise It will confused .
