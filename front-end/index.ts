bf5c39ca91214b6efbabb95e72cbd6b258081aab
Hiroji <joegilbertdev@outlook.com>
2022-11-09 15:45:41 +0000

    changed file names

--- front-end/index.ts
+++ front-end/index.ts
@@ -21,6 +21,6 @@ $(document).on("click", "#new_medicine_form_button", (e) => {
   let row__id = `${count}__table__row`
 
   $("#current__medicines__table > tbody:first").append(
-    `<tr class="medicines__table__row" id=${row__id} ><td>${newMedicine}</td><td>${newQuantity}</td><td>${newDose}</td></tr>`
+    `<tr class="medicines__table__row" id=${row__id} ><td>${newMedicine}</td><td>${newQuantity}</td><td>${newDose}</td><td><button class="delete__btn">Remove</button></td></tr>`
   );
 });
