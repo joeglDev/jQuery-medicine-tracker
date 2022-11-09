8c0c41abff83eb8d24bc5767b4dc0cbae94fe9a3
Hiroji <joegilbertdev@outlook.com>
2022-11-09 16:11:46 +0000

    working delete medication button fucntionality

--- front-end/index.ts
+++ front-end/index.ts
@@ -9,6 +9,8 @@ $(() => {
 
 //medicines__list code
 let count = 0;
+
+//add new table row
 $(document).on("click", "#new_medicine_form_button", (e) => {
   e.preventDefault();
   //get new values
@@ -18,9 +20,25 @@ $(document).on("click", "#new_medicine_form_button", (e) => {
 
   //generate unique row ids
   count++;
-  let row__id = `${count}__table__row`
 
   $("#current__medicines__table > tbody:first").append(
-    `<tr class="medicines__table__row" id=${row__id} ><td>${newMedicine}</td><td>${newQuantity}</td><td>${newDose}</td><td><button class="delete__btn">Remove</button></td></tr>`
+    `<tr class="medicines__table__row" id="${count}__table__row" ><td>${newMedicine}</td><td>${newQuantity}</td><td>${newDose}</td><td><button id="${count}__delete__btn" class="delete__btn">Remove</button></td></tr>`
   );
 });
+
+//remove table row
+//note -> did not use ES6 arrow func as could not use this keyword to grab button id
+$(document).on(
+  "click",
+  "[id*=__delete__btn]",
+  function () {
+    const rowID = $(this).parent().parent().attr("id");
+    if (rowID) {
+      $(`#${rowID}`).remove();
+    }
+  }
+
+  //get id of clicked
+  //id of parent
+  //remove parent
+);
